bd11ac84eb462570e6fe02d61b436e55f7915a89
LuckyBelieve <luckybelieve@gmail.com>
2023-07-27 14:01:30 +0200

    user profile completion

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,9 @@
 "use client";
+
 import React from "react";
 import { useState, useEffect } from "react";
 import PromptCard from "./PromptCard";
+
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
@@ -15,12 +17,13 @@ const PromptCardList = ({ data, handleTagClick }) => {
     </div>
   );
 };
+
 const Feed = () => {
-  const [searchText, setSearchText] = useState("");
   const [posts, setPosts] = useState([]);
-  const handlesearchChange = async (e) => {
-    e.preventDefault();
-  };
+  const [searchText, setSearchText] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(null);
+  const [searchedResults, setSearchedResults] = useState([]);
+
   useEffect(() => {
     const fetchPost = async () => {
       const response = await fetch("/api/prompt");
@@ -29,6 +32,32 @@ const Feed = () => {
     };
     fetchPost();
   }, []);
+
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i");
+    return posts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = async (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
@@ -36,11 +65,18 @@ const Feed = () => {
           type="text"
           placeholder="search for a tag a username"
           value={searchText}
-          onChange={handlesearchChange}
+          onChange={handleSearchChange}
           className="search_input peer"
         />
       </form>
-      <PromptCardList data={posts} handleTagClick={() => {}} />
+      {searchText ? (
+        <PromptCardList
+          data={searchedResults}
+          handleTagClick={handleTagClick}
+        />
+      ) : (
+        <PromptCardList data={posts} handleTagClick={handleTagClick} />
+      )}
     </section>
   );
 };
